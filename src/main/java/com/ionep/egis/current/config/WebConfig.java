a614418c300e40aecfe67822c1719df44e80a698
Ted Velkoff <ted.velkoff@ionep.com>
2019-01-27 23:42:03 -0500

    Add scheduler

--- src/main/java/com/ionep/egis/current/config/WebConfig.java
+++ src/main/java/com/ionep/egis/current/config/WebConfig.java
@@ -1,11 +1,22 @@
 package com.ionep.egis.current.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebMvc
+@EnableScheduling
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public ThreadPoolTaskScheduler scheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(10);
+		return scheduler;
+	}
 
 }

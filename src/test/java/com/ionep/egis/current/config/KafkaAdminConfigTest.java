da3c33aea5f41267ca33637bb0fa9f140cd709ff
Ted Velkoff <ted.velkoff@ionep.com>
2019-02-02 12:17:33 -0500

    Update with SpringExtension

--- src/test/java/com/ionep/egis/current/config/KafkaAdminConfigTest.java
+++ src/test/java/com/ionep/egis/current/config/KafkaAdminConfigTest.java
@@ -3,9 +3,11 @@ package com.ionep.egis.current.config;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+//@ExtendWith(SpringExtension.class)
 public class KafkaAdminConfigTest {
 	
 	@Test
